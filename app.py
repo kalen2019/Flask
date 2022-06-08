fc0968b0d797e92b4e1b7f5199b7945ea2da0753
kalen2019 <leo201902@gmail.com>
2022-06-08 15:37:06 +0800

    Add unit test with unittest

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 
 import os
 import sys
